c3a1735c655528f968ea68d5a9312305cee53706
KaosSpectre <97178572+KaosSpectre@users.noreply.github.com>
2022-11-16 20:28:30 -0500

    Initial File commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const lengthEl = document.getElementById("text-length");
+const volumeEl = document.getElementById("text-volume");
+const massEl = document.getElementById("text-mass");
+const inputEl = document.getElementById("to-convert");
+inputEl.addEventListener("keyup", function () {});
